f7a4f7ec40c8050041ba4facba7e0ca0d0432bf5
a18216891 <devpavdeveloper@gmail.com>
2020-12-27 19:03:11 +0300

    Add in api layout module

--- projects/devpav-angular/src/public-api.ts
+++ projects/devpav-angular/src/public-api.ts
@@ -9,6 +9,7 @@ export * from './lib/ngx-devpav-form-input/ngx-devpav-form-input.module';
 export * from './lib/ngx-devpav-button/ngx-devpav-button.module';
 export * from './lib/ngx-devpav-calendar/ngx-devpav-calendar.module';
 export * from './lib/ngx-devpav-list/ngx-devpav-list.module';
+export * from './lib/ngx-devpav-layout/ngx-devpav-layout.module';
 
 // services
 export * from './lib/ngx-devpav-theme/ngx-devpav-theme.service';
