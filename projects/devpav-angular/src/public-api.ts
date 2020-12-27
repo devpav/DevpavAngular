3e23bc156f8e364d5f709d4048121c98ce0761d3
a18216891 <devpavdeveloper@gmail.com>
2020-12-28 02:45:33 +0300

    Change color panel and add panel flop to open api

--- projects/devpav-angular/src/public-api.ts
+++ projects/devpav-angular/src/public-api.ts
@@ -11,6 +11,8 @@ export * from './lib/ngx-devpav-calendar/ngx-devpav-calendar.module';
 export * from './lib/ngx-devpav-list/ngx-devpav-list.module';
 export * from './lib/ngx-devpav-layout/ngx-devpav-layout.module';
 export * from './lib/ngx-devpav-selector/ngx-devpav-selector.module';
+export * from './lib/ngx-devpav-loader/ngx-devpav-loader.module';
+export * from './lib/ngx-devpav-flop-panel/ngx-devpav-flop-panel.module';
 
 // services
 export * from './lib/ngx-devpav-theme/ngx-devpav-theme.service';
