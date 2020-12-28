82689053b538cee7f90d2fe655cc1b04425ce229
a18216891 <devpavdeveloper@gmail.com>
2020-12-28 04:31:51 +0300

    Add css bundle library

--- projects/devpav-angular/src/lib/ngx-devpav-theme/ngx-devpav-theme.module.ts
+++ projects/devpav-angular/src/lib/ngx-devpav-theme/ngx-devpav-theme.module.ts
@@ -1,7 +1,24 @@
-import {NgModule} from '@angular/core';
+import {ModuleWithProviders, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NgxDevpavThemeService} from './ngx-devpav-theme.service';
+import {Theme} from '../theme.config';
 
+export interface NgxThemeOptions {
+  themes: Theme[];
+  active: Theme;
+}
+
+const ngxThemeFactory = (ngxThemeOption: NgxThemeOptions): NgxDevpavThemeService => {
+  const ngxDevpavThemeService = new NgxDevpavThemeService();
+
+  if (ngxThemeOption.themes) {
+    ngxThemeOption.themes.forEach(ngxDevpavThemeService.register);
+  }
+
+  ngxDevpavThemeService.applyTheme(ngxThemeOption.active);
+
+  return (ngxDevpavThemeService);
+};
 
 @NgModule({
   declarations: [],
@@ -13,4 +30,17 @@ import {NgxDevpavThemeService} from './ngx-devpav-theme.service';
   ]
 })
 export class NgxDevpavThemeModule {
+
+  static forRoot(options: NgxThemeOptions): ModuleWithProviders {
+    return {
+      ngModule: NgxDevpavThemeModule,
+      providers: [
+        {
+          provide: NgxDevpavThemeService,
+          useFactory: ngxThemeFactory
+        }
+      ]
+    };
+  }
+
 }
