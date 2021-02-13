dff47173222a5f7c0e6175160e2759f4d77fb85d
Талайко Павел Дмитриевич <devpavdeveloper@gmail.com>
2021-02-14 01:38:37 +0300

    change version

--- projects/devpav-angular/src/lib/ngx-devpav-selector/ngx-devpav-singleton-selecor/ngx-devpav-selector.component.ts
+++ projects/devpav-angular/src/lib/ngx-devpav-selector/ngx-devpav-singleton-selecor/ngx-devpav-selector.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {dropdownAnimation, dropdownSelectorAnimation} from '../../ngx-devpav-animations/dropdown-animation';
 import {ListOption} from '../../ngx-devpav-list/ngx-devpav-list/ngx-devpav-list.component';
+import {dropdownSelectorAnimation} from '../../ngx-devpav-animations/dropdown-animation';
 
 
 @Component({
